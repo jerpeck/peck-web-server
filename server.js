900c95a1675f8b4878984187cff99e2879ff5df6
Jeremiah Peck <jerpeck77@gmail.com>
2021-08-17 22:24:00 -0700

    added port variable

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ transporter.sendMail(mailOptions, function (err, data) {
 });
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
